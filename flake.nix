192e4ec3055fa8e3a256e31c3e4507ea27be1bb9
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2024-09-11 11:05:13 +0530

    feat: initialize go project
    Signed-off-by: Murtaza Udaipurwala <murtaza@murtazau.xyz>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Accuknox Kubernetes-native system metrics reporter";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            nixpkgs-fmt
+            go
+            go-tools
+            gopls
+            templ
+            air
+            nodejs
+            nodePackages.pnpm
+            nodePackages.vscode-langservers-extracted
+            nodePackages.typescript-language-server
+            tailwindcss-language-server
+            emmet-language-server
+            prettierd
+            kubectl
+            kind
+            kubernetes-helm
+          ];
+        };
+      });
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/murtaza-u/rinc
+
+go 1.22.6
